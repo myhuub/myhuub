3d68eca0bc389af7c50a694fb8be9ef2352d09f1
myhuub <93978100+myhuub@users.noreply.github.com>
2021-11-18 14:16:41 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+import React from "react";
+import {View, Text,TextInput} from "react-native";
+import { WebView } from 'react-native-webview';
 export const applyCustomCode = externalCodeSetup => {
-	// call custom code api here
+    // call custom code api here
+    // externalCodeSetup.configApi.setAppSwitchEnabled(true);
+    externalCodeSetup.navigationApi.replaceScreenComponent("SignupScreen", () => (
+        <WebView source={{ uri: 'https://mesa.myhuub.com/register/' }} />
+    ));
 };
